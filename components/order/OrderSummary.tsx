2031f8d6a3ee1e3e44649cea2e031b375ade61c3
juansefdz <jsfm81091@gmail.com>
2024-09-11 18:35:25 -0500

    order management completed

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,9 +1,16 @@
 "use client";
 import { useStore } from "@/src/store";
 import ProductDetails from "./ProductDetails";
+import { useMemo } from "react";
+import { formatCurrency } from "@/src/utils";
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order);
+  const total = useMemo(
+    () => order.reduce((total, item) => total + item.quantity * item.price, 0),
+    [order]
+  );
+
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">MI PEDIDO </h1>
@@ -13,10 +20,13 @@ export default function OrderSummary() {
       ) : (
         <div className="mt-5">
           {order.map((item) => (
-            <ProductDetails 
-            key={item.id} 
-            item={item}/>
+            <ProductDetails key={item.id} item={item} />
           ))}
+
+          <p className="text-2xl mt-20 text-center">
+            Total a pagar:{" "}
+            <span className="font-bold">{formatCurrency(total)}</span>
+          </p>
         </div>
       )}
     </aside>
