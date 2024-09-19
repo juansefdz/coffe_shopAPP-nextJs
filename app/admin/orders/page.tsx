f7460593823c47369c5f2003290270fba667f111
juansefdz <jsfm81091@gmail.com>
2024-09-18 19:10:21 -0500

    orders administration completed

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,40 +1,39 @@
+"use client";
+
+import useSWR from "swr";
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
+import { OrderWithProducts } from "@/src/types";
 
-async function getPendingOrders() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false,
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true,
-        },
-      },
-    },
+export default function AdminOrdersPage() {
+  const url = "/admin/orders/api";
+
+  const fetcher = () =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => data);
+
+  const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 60000,
+    revalidateOnFocus:false
   });
 
-  return orders;
-}
-export default async function AdminOrdersPage() {
-  const orders = await getPendingOrders();
-  console.log(orders);
-  return (
-    <>
-      <Heading>Administrar ordenes</Heading>
-      {orders.length ? (
-        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map((order) => (
-            <OrderCard
-              key={order.id} 
-              order={order}/>
-          ))}
-        </div>
-      ) : (
-        <p className="text-center">No hay ordenes pendientes</p>
-      )}
-    </>
-  );
+  if (isLoading) return <p>Cargando...</p>;
+
+  if (data)
+    return (
+      <>
+        <Heading>Administrar ordenes</Heading>
+
+        {data.length ? (
+          <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+            {data.map((order) => (
+              <OrderCard key={order.id} order={order} />
+            ))}
+          </div>
+        ) : (
+          <p className="text-center">No hay ordenes pendientes</p>
+        )}
+      </>
+    );
 }
